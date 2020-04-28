87b49fc7fe798ec9cc21d473e416d718cabb8775
Archana <archana@IN-ABRAMEGOWDA-MBP.local>
2020-04-28 21:26:51 +0530

    new MSA form implementation

--- Sign-up-confirmation/Landing/MSA-Sign-Upload/msaSignUpload.js
+++ Sign-up-confirmation/Landing/MSA-Sign-Upload/msaSignUpload.js
@@ -6,11 +6,11 @@ window.onload = function() {
     localStorage.setItem("msa", "false");
   }
 
-  if (document.getElementById("myfile").value == "") {
+  if (document.getElementById("myfile").value == "" || document.getElementById("fileElem").value == "") {
     localStorage.setItem("msa", "false");
   }
 
-  if (document.getElementById("myfile").value != "") {
+  if (document.getElementById("myfile").value != "" || document.getElementById("fileElem").value != "") {
     localStorage.setItem("msa", "true");
     enableSubmitButton();
   } else {
@@ -23,10 +23,8 @@ function setMSAKey() {
   localStorage.setItem("msa", "true");
 }
 
-function uploadFile() {
-  if (document.getElementById("myfile").value != "") {
-    enableSubmitButton();
-  }
+function uploadFile(file) {
+  previewFile(file)
 }
 
 function disableSubmitButton() {
@@ -45,7 +43,48 @@ function enableSubmitButton() {
   document.getElementById("msa-submit").removeAttribute("disabled");
 }
 
-function printPDF() {
-  window.frames["myFrame"].focus();
-  window.frames["myFrame"].print();
+// ************************ Drag and drop ***************** //
+let dropArea = document.getElementById("drop-area");
+
+// Prevent default drag behaviors
+["dragenter", "dragover", "dragleave", "drop"].forEach((eventName) => {
+  dropArea.addEventListener(eventName, preventDefaults, false);
+  document.body.addEventListener(eventName, preventDefaults, false);
+});
+
+// Highlight drop area when item is dragged over it
+["dragenter", "dragover"].forEach((eventName) => {
+  dropArea.addEventListener(eventName, highlight, false);
+});
+["dragleave", "drop"].forEach((eventName) => {
+  dropArea.addEventListener(eventName, unhighlight, false);
+});
+
+// Handle dropped files
+dropArea.addEventListener("drop", handleDrop, false);
+
+function preventDefaults(e) {
+  e.preventDefault();
+  e.stopPropagation();
+}
+
+function highlight(e) {
+  dropArea.classList.add("highlight");
+}
+
+function unhighlight(e) {
+  dropArea.classList.remove("highlight");
+}
+
+function handleDrop(e) {
+  e.preventDefault();
+  var file = e.dataTransfer.files[0];
+  previewFile(file);
+}
+
+function previewFile(file) {
+  document.getElementById("gallery").innerHTML = file.name;
+  if (file) {
+    enableSubmitButton();
+  }
 }
